f47f5d0125663abf32029b3c5efb9e2433a7d72d
sho.mishima <haskell.raspberry@gmail.com>
2020-11-10 14:03:26 +0900

    Updates

---
+++ precache-manifest.8b99afa44ddc9bfe9614736c8ea6ddc9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ad248088e9ad085c361dd088f039a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "632a0494ff9a77bb7d3f",
+    "url": "/static/css/main.34fc1070.chunk.css"
+  },
+  {
+    "revision": "cef686f99eea0353e620",
+    "url": "/static/js/2.612274d4.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.612274d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632a0494ff9a77bb7d3f",
+    "url": "/static/js/main.bad2b52a.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "3d65e9c9d33d04547e17e165e68f6fe3",
+    "url": "/static/media/auto_door.3d65e9c9.png"
+  },
+  {
+    "revision": "3f0095c8479c7d53174ad485ccce689c",
+    "url": "/static/media/automaton.3f0095c8.png"
+  },
+  {
+    "revision": "2244983702d9cba5736ca11fe1ebd1a6",
+    "url": "/static/media/automaton_101.22449837.gif"
+  },
+  {
+    "revision": "ec6ff9be9beee9c96189065481391594",
+    "url": "/static/media/change_state.ec6ff9be.gif"
+  },
+  {
+    "revision": "6c30c740e076e03a69d6df054deb5565",
+    "url": "/static/media/create_transition.6c30c740.gif"
+  },
+  {
+    "revision": "64cfd0819ab15079863d330fadb27a66",
+    "url": "/static/media/delete_state.64cfd081.gif"
+  },
+  {
+    "revision": "407e87a55aaa7d8fa30a3af490533ea5",
+    "url": "/static/media/delete_transition.407e87a5.gif"
+  },
+  {
+    "revision": "cb94b5b629fbe7a0cf091999ed433270",
+    "url": "/static/media/duplicate_label.cb94b5b6.png"
+  },
+  {
+    "revision": "7d887b6169503a43808beccfea91279d",
+    "url": "/static/media/elevator.7d887b61.png"
+  },
+  {
+    "revision": "bbd1426a8ebf0cfb6244af493be6c40e",
+    "url": "/static/media/input_transition_label.bbd1426a.gif"
+  },
+  {
+    "revision": "fe5f234bbbf221a9ecd97b597adc2bed",
+    "url": "/static/media/multi_label.fe5f234b.png"
+  },
+  {
+    "revision": "b03505792ef07d66ed59ebc6219b2012",
+    "url": "/static/media/no_label.b0350579.png"
+  },
+  {
+    "revision": "fe2c5e963c1bc740485300e7b3548ebe",
+    "url": "/static/media/play.fe2c5e96.gif"
+  },
+  {
+    "revision": "bd51a445e8ab8afb5898e4d415fb70b1",
+    "url": "/static/media/play_screen_structure.bd51a445.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/automaton-app/precache-manifest.a6e00f193962796d1586c39860847695.js"
+  "/precache-manifest.8b99afa44ddc9bfe9614736c8ea6ddc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/automaton-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
